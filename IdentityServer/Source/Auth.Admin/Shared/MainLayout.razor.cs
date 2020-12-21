46f142ab36d8d7be2efc67203c41d17e2e544baf
SideswipeN7 <pawel.wanot@gmail.com>
2020-12-21 10:28:58 +0100

    Add blazor app with MatBlazor lib

---
+++ IdentityServer/Source/Auth.Admin/Shared/MainLayout.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Auth.Admin.Shared
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        protected const string AppName = "Auth Admin";
+
+        protected bool Opened { get; set; }
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; } = default!;
+
+        protected void ToggleDrawer() => Opened = !Opened;
+
+        protected void NavigateHome() => NavigationManager.NavigateTo("");
+    }
+}
